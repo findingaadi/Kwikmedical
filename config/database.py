6d678d0301f47bf6287d171ebaa5d0024c7e87a3
findingaadi <adarshaaryal30@gmail.com>
2024-11-14 19:02:37 +0000

    first commit

---
+++ config/database.py
@@ -0,0 +1,18 @@
+import cx_Oracle
+import os
+from dotenv import load_dotenv
+
+
+def get_db_connection():
+    try:
+        dsn = cx_Oracle.makedsn(os.getenv("DB_HOST"), os.getenv("DB_PORT"), sid=os.getenv("DB_SID"))
+        connection = cx_Oracle.connect(
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            dsn=dsn
+        )
+        print("connected")
+        return connection
+    except cx_Oracle.DatabaseError as e:
+        print("Error while connecting to Oracle Database", e)
+        return None
